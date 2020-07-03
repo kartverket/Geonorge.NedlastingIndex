15b80fcae31110176de21757a35c4930bae5b749
Dag Olav Dahle <dagolav@arkitektum.no>
2020-07-03 10:56:41 +0200

    Add visual studio solution

---
+++ Geonorge.NedlastingIndex/Controllers/SearchIndexController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Geonorge.NedlastingIndex.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SearchIndexController : ControllerBase
+    {
+        private readonly ILogger<SearchIndexController> _logger;
+
+        public SearchIndexController(ILogger<SearchIndexController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "searchindex";
+        }
+    }
+}
